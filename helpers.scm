3e6e0b7e122382ca92097246a05ee45297828caf
Isaac Hiram Lopez Diaz <ih1d@loki>
2025-02-25 14:31:59 -0500

    initial commit

---
+++ helpers.scm
@@ -0,0 +1,16 @@
+;; Helper functions from The Little Schemer
+;; Author: Isaac H. Lopez Diaz
+
+;; Determines if the expression passed is an atom
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+;; Increases the numner passed by one
+(define (add1 x)
+  (+ x 1))
+
+;; Decreases the number passed by one
+(define sub1
+  (lambda (x)
+    (- x 1)))
